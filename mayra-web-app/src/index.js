6ff3b6ee7af7f8eca2672ebe5cc0018008739ce7
Efgon <egonzalezgar@gmail.com>
2021-11-01 12:55:01 -0400

    added hompeage

--- mayra-web-app/src/index.js
+++ mayra-web-app/src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="">
+    <BrowserRouter basename="MayraMaldonado">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
