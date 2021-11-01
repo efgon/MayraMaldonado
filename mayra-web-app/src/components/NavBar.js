59b2673474480c8b894a213ac330823a6e665331
Efgon <egonzalezgar@gmail.com>
2021-11-01 13:22:15 -0400

    hot fixes

--- mayra-web-app/src/components/NavBar.js
+++ mayra-web-app/src/components/NavBar.js
@@ -10,11 +10,11 @@ function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link to="/MayraMaldonado">Inicio</Nav.Link>
-            <Nav.Link to="/SobreMi">Trayectoria</Nav.Link>
-            <Nav.Link to="/QueOfrecemos">Ofrecimientos</Nav.Link>
-            <Nav.Link to="/MiLibro">Libros</Nav.Link>
-            <Nav.Link to="/Podcast">Podcast</Nav.Link>
+            <Nav.Link href="/MayraMaldonado">Inicio</Nav.Link>
+            <Nav.Link href="/SobreMi">Trayectoria</Nav.Link>
+            <Nav.Link href="/QueOfrecemos">Ofrecimientos</Nav.Link>
+            <Nav.Link href="/MiLibro">Libros</Nav.Link>
+            <Nav.Link href="/Podcast">Podcast</Nav.Link>
             <NavDropdown title="Mas" id="basic-nav-dropdown">
               <NavDropdown.Item href="/Contactos">Contactos</NavDropdown.Item>
               <NavDropdown.Divider />
