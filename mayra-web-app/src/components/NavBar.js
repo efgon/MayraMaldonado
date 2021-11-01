06005b6affa14b0485e5f84ac66d98c75d223c99
Efgon <egonzalezgar@gmail.com>
2021-11-01 11:09:57 -0400

    first draft for this project

---
+++ mayra-web-app/src/components/NavBar.js
@@ -0,0 +1,27 @@
+import { Navbar, NavDropdown, Container, Nav } from "react-bootstrap";
+
+function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">Mayra Maldonado Brignoni</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Inicio</Nav.Link>
+            <Nav.Link href="/SobreMi">Trayectoria</Nav.Link>
+            <Nav.Link href="/QueOfrecemos">Ofrecimientos</Nav.Link>
+            <Nav.Link href="/MiLibro">Libros</Nav.Link>
+            <Nav.Link href="/Podcast">Podcast</Nav.Link>
+            <NavDropdown title="Mas" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/Contactos">Contactos</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/Noticias">Noticias</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+export default NavBar;
